4396f742548857e26045e2fff2a06e49a52660cb
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-03-21 20:17:05 +0200

    ready

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import { Formik, Field } from 'formik';
+import { Form, FormField, ErrorMessage, Button } from './ContactForm.styled';
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export const ContactForm = ({ onSubmit }) => {
+  const handleSubmit = (values, { resetForm }) => {
+    onSubmit(values);
+    resetForm();
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form>
+        <FormField>
+          Name
+          <Field
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <ErrorMessage name="name" component="div" />
+        </FormField>
+        <FormField>
+          Phone
+          <Field
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </FormField>
+        <ErrorMessage name="number" component="div" />
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </Formik>
+  );
+};
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
